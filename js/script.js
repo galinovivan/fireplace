290d13d336fd577fe2ecdab2cb1b2eabbd407bfe
ivangalinov <9225913@gmail.com>
2016-09-12 16:45:02 +0300

    add full_width vers

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    
+   initHideELement($('#topNav'), $('#navToggle'), $('#navClose'), 'active');
+   
+    function initHideELement(element, openToggle, closeToggle, newClass) {
+        function initCollapse() {
+        if (element.hasClass(newClass)) {
+            element.removeClass(newClass);
+        } else {
+            element.addClass(newClass);
+            };
+        }
+        openToggle.click(function(event) {
+            initCollapse();
+            event.preventDefault();
+        });
+        openToggle.on('tap', function(event)  {
+            initCollapse();
+            event.preventDefault();
+        });
+        if (closeToggle != false) {
+            closeToggle.click(function(event) {
+                initCollapse();
+                event.preventDefault();
+            });
+            closeToggle.on('tap', function(event) {
+                initCollapse();
+                event.preventDefault(event);
+            });
+        };
+    }
+});
