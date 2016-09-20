97071abe1f862ed9a6e6358a8f16edbb0bcad25f
ivangalinov <9225913@gmail.com>
2016-09-20 19:22:31 +0300

    succesfull poor size

--- js/poor_vision.js
+++ js/poor_vision.js
@@ -1,14 +1,74 @@
 $(document).ready(function() {
-    if ($.cookie("cecutientCookie") == "on") {
+    if ($.cookie("poor_vision") == "on") {
        $('html').addClass('poor_vision');
+        $('#poorVision').hide();
+        $('#poorPanel').show();
         if ($.cookie("fonts") == "small") {
-            $('body').addClass('small_size');
+            toSmallSize();
         }
         if ($.cookie("fonts") == "medium") {
-            $('body').addClass('medium_size');
+            toMediumSize();
         }
         if ($.cookie("fonts") == "large") {
-            $('body').addClass('large_size');
+           toLargeSize();
         }
+    } else if ($.cookie("poor_vision") == "off" ||  $.cookie("poor_vision") == undefined) {
+        $('#poorPanel').hide();
+        $('#poorVision').show();
+    }
+    var htmlPage = $('html');
+    var body = $('body');
+
+    $('#poorVision').click(function(event) {
+            htmlPage.addClass('poor_vision');
+           $.cookie("poor_vision", "on");
+            $(this).hide();
+           $('#poorPanel').show();
+           event.preventDefault();
+    });
+    $('#normalVision').click(function(event) {
+        htmlPage.removeClass('poor_vision');
+        $.cookie("poor_vision", "off");
+        $('#poorPanel').hide();
+        $('#poorVision').show();
+        event.preventDefault();
+    });
+    $('#largeSize').click(function(event) {
+        $.cookie("fonts", "large");
+       toLargeSize();
+        event.preventDefault();
+    });
+    $('#mediumSize').click(function(event) {
+        $.cookie("fonts", "medium");
+        toMediumSize();
+        event.preventDefault();
+    });
+
+    $('#smallSize').click(function(event) {
+        $.cookie("fonts", "small");
+        toSmallSize();
+        event.preventDefault();
+    });
+
+    $('.resize_btn').click(function(event) {
+       $('.resize_btn').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+
+    function toLargeSize() {
+        var body = $('body');
+        body.removeClass();
+        body.addClass('large_size');
+    }
+    function toMediumSize() {
+        var body = $('body');
+        body.removeClass();
+        body.addClass('medium_size');
+    }
+
+    function toSmallSize() {
+        var body = $('body');
+        body.removeClass();
     }
 });
